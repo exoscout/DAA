75017992418fd722b1fc18aca141130f4edcdba3
exoscout <exoscout@gmail.com>
2026-03-16 01:11:48 +0530

    Otsu Code+implemented

---
+++ Otsu/OTSU.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+
+image = cv2.imread("medical_image.jpg", 0)
+
+
+ret, segmented = cv2.threshold(
+    image,
+    0,
+    255,
+    cv2.THRESH_BINARY + cv2.THRESH_OTSU
+)
+
+cv2.imwrite("otsu_result.png", segmented)
+
+
+print("Optimal Threshold:", ret)
